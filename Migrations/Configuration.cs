83254e1c09443d8a4780e5f66e4143f86c20a6e9
mdnabeel <md.nabeel786@gmail.com>
2020-05-04 18:06:43 +0530

    Updating The Domain Model

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace VidlyWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VidlyWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VidlyWebApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- Models/Customers.cs
+++ Models/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace VidlyWebApp.Models
     public class Customers
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(255)]
         public String CustomerName { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
